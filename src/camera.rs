01321e1d7f52d45d68b6ae54b9c5e3d2ee75a1c9
danielbartsch <daniel.bartsch@adverity.com>
2020-01-08 21:37:48 +0100

    added scale to camera for zoom feature

--- src/camera.rs
+++ src/camera.rs
@@ -4,6 +4,7 @@ pub mod camera {
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Camera {
         pub position: (i32, i32),
+        pub scale: (f32, f32),
         width: u16,
         height: u16,
     }
@@ -11,6 +12,7 @@ pub mod camera {
         pub fn new(width: u16, height: u16) -> Camera {
             Camera {
                 position: (0, 0),
+                scale: (1.0, 1.0),
                 width,
                 height,
             }
@@ -20,6 +22,12 @@ pub mod camera {
         }
         pub fn get_y(&self) -> i32 {
             self.position.1
+        }
+        pub fn get_scale_x(&self) -> f32 {
+            self.scale.0
+        }
+        pub fn get_scale_y(&self) -> f32 {
+            self.scale.1
         }
         pub fn to_target(&mut self, target_position: (i32, i32), rate: f32) {
             self.position.0 += ((target_position.0 - self.get_x()) as f32 * rate) as i32;
