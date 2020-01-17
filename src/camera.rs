03cd1c7a0204098fb72aff29298dfdbfbb96e6b4
danielbartsch <daniel.bartsch@adverity.com>
2020-01-17 19:09:31 +0100

    added max and min zoom levels

--- src/camera.rs
+++ src/camera.rs
@@ -28,14 +28,30 @@ impl Camera {
     pub fn get_scale_y(&self) -> f32 {
         self.scale.1
     }
+    fn restrict_zoom(&mut self) {
+        if self.scale.0 > 25.0 {
+            self.scale.0 = 25.0
+        }
+        if self.scale.1 > 25.0 {
+            self.scale.1 = 25.0
+        }
+        if self.scale.0 < 0.01 {
+            self.scale.0 = 0.01
+        }
+        if self.scale.1 < 0.01 {
+            self.scale.1 = 0.01
+        }
+    }
     pub fn zoom(&mut self, scale: f32) {
         self.scale.0 *= scale;
         self.scale.1 *= scale;
+        self.restrict_zoom();
     }
     pub fn set_zoom(&mut self, scale: f32) {
         let difference = (self.scale.0 / scale, self.scale.1 / scale);
         self.scale.0 = scale;
         self.scale.1 = scale;
+        self.restrict_zoom();
         self.position.0 *= difference.0;
         self.position.1 *= difference.1;
     }
